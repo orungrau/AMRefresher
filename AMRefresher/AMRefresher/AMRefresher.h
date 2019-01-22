a5fdb50ac7ca7383fa0e5a79d5eb7eeee4814e59
Anatoly Myaskov <myaskov@me.com>
2019-01-22 18:03:10 +0300

    Add Carthage support

---
+++ AMRefresher/AMRefresher/AMRefresher.h
@@ -0,0 +1,19 @@
+//
+//  AMRefresher.h
+//  AMRefresher
+//
+//  Created by Anatoly Myaskov on 1/22/19.
+//  Copyright © 2019 Anatoly Myaskov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMRefresher.
+FOUNDATION_EXPORT double AMRefresherVersionNumber;
+
+//! Project version string for AMRefresher.
+FOUNDATION_EXPORT const unsigned char AMRefresherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMRefresher/PublicHeader.h>
+
+
